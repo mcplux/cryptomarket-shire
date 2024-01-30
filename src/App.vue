a6124833519fa7d032c0f659f5d417bddb2f3bf5
Juan Martinez <pablomar01@outlook.com>
2024-01-30 11:34:05 -0500

    active dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
+import { useDarkModeStore } from '@/stores/darkMode'
+
+const darkMode = useDarkModeStore()
 </script>
 
 <template>
-  <RouterView />
+  <div :class="{'dark': darkMode.isActived}">
+    <div class="bg-gray-200 dark:bg-slate-700 min-h-screen">
+      
+
+      <RouterView />
+    </div>
+  </div>
 </template>
---
+++ src/stores/darkMode.js
@@ -0,0 +1,23 @@
+import { computed, onMounted, ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useDarkModeStore = defineStore('dark-mode', () => {
+  const dark = ref(false)
+
+  onMounted(() => {
+    if(localStorage.getItem('userPreferences')) {
+      dark.value = JSON.parse(localStorage.getItem('userPreferences')).darkMode
+    } else {
+      dark.value = window.matchMedia('(prefers-color-scheme: dark)').matches
+      localStorage.setItem('userPreferences', JSON.stringify({darkMode: dark.value}))
+    }
+
+  })
+
+  const isActived = computed(() => dark)
+
+  return {
+    dark,
+    isActived,
+  }
+})
