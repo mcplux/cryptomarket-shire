85d2fd5acb59e87539b0f23c257a5ac68bad3bc8
Juan Martinez <pablomar01@outlook.com>
2024-03-18 11:33:59 -0500

    import language according to user preferences

--- src/stores/userPreferences.js
+++ src/stores/userPreferences.js
@@ -3,43 +3,52 @@ import { defineStore } from 'pinia'
 
 export const useUserPreferencesStore = defineStore('user-preferences', () => {
   const darkMode = ref(false)
-  const lang = ref('')
+  const language = ref('')
+  const lang = ({})
   const supportedLangs = ref({
-    es: 'Español',
     en: 'English',
   })
 
   onMounted(() => {
     if(localStorage.getItem('userPreferences')) {
-      lang.value = JSON.parse(localStorage.getItem('userPreferences')).lang ?? 'en'
-      darkMode.value = JSON.parse(localStorage.getItem('userPreferences')).darkMode ?? false
+      language.value = JSON.parse(localStorage.getItem('userPreferences')).lang ?? 'en'
+      importLanguage()
 
-      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
+      darkMode.value = JSON.parse(localStorage.getItem('userPreferences')).darkMode ?? false
+      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: language.value}))
     } else {
       const language = (navigator.language || 'en').substring(0, 2)
+      language.value = Object.keys(supportedLangs.value).includes(language) ? language : 'en'
+      importLanguage()
 
-      lang.value = Object.keys(supportedLangs.value).includes(language) ? language : 'en'
       darkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches
-      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
-    }
 
+      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: language.value}))
+    }
   })
+
+  function importLanguage() {
+    fetch(`locale/${language.value}.json`)
+      .then(response => response.json())
+      .then(data => lang.value = data)
+      .catch(err => console.error(err))
+  }
 
   function updateDarkMode() {
     darkMode.value = !darkMode.value
-    localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
+    localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: language.value}))
   }
 
   function updateLanguage(newLang) {
-    lang.value = Object.keys(supportedLangs.value).includes(newLang) ? newLang : lang.value
-    localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
+    language.value = Object.keys(supportedLangs.value).includes(newLang) ? newLang : language.value
+    importLanguage()
+    localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: language.value}))
   }
 
   const isDarkModeActivated = computed(() => darkMode.value)
-  const currentLanguage = computed(() => supportedLangs.value[lang.value])
+  const currentLanguage = computed(() => supportedLangs.value[language.value])
 
   return {
-    darkMode,
     supportedLangs,
     updateDarkMode,
     updateLanguage,
