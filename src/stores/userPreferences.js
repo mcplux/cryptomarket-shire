aeaa999ebd941dc22f0ddd6dec7ead5c8d0a0a82
Juan Martinez <pablomar01@outlook.com>
2024-03-18 10:33:14 -0500

    load language settings from localStorage or browser settings

--- src/stores/userPreferences.js
+++ src/stores/userPreferences.js
@@ -3,13 +3,21 @@ import { defineStore } from 'pinia'
 
 export const useUserPreferencesStore = defineStore('user-preferences', () => {
   const darkMode = ref(false)
+  const lang = ref('')
+  const supportedLangs = ref(['es', 'en'])
 
   onMounted(() => {
     if(localStorage.getItem('userPreferences')) {
+      lang.value = JSON.parse(localStorage.getItem('userPreferences')).lang ?? 'en'
       darkMode.value = JSON.parse(localStorage.getItem('userPreferences')).darkMode ?? false
+
+      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
     } else {
+      const language = (navigator.language || 'en').substring(0, 2)
+
+      lang.value = supportedLangs.value.includes(language) ? language : 'es'
       darkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches
-      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value}))
+      localStorage.setItem('userPreferences', JSON.stringify({darkMode: darkMode.value, lang: lang.value}))
     }
 
   })
