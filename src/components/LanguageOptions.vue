0f15757e45e36bd5870b0385c1b053fa35cc86d9
Juan Martinez <pablomar01@outlook.com>
2024-03-18 14:18:37 -0500

    change dropdown menu colors

--- src/components/LanguageOptions.vue
+++ src/components/LanguageOptions.vue
@@ -11,7 +11,7 @@ const userPreferences = useUserPreferencesStore()
     <Menu as="div" class="relative inline-block text-left">
       <div>
         <MenuButton
-          class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+          class="inline-flex w-full justify-center rounded-md dark:bg-black bg-blue-800 px-4 py-2 text-sm font-medium text-white hover:opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
         >
           {{ userPreferences.currentLanguage }}
           <ChevronDownIcon />
@@ -27,13 +27,14 @@ const userPreferences = useUserPreferencesStore()
         leave-to-class="transform scale-95 opacity-0"
       >
         <MenuItems
-          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+          class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-gray-200 dark:bg-gray-500 shadow-lg ring-1 ring-black/5 focus:outline-none"
         >
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }" v-for="lang in Object.keys(userPreferences.supportedLangs)">
               <button
+                class="text-gray-900 dark:text-white"
                 :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                  active ? 'bg-blue-500 dark:bg-slate-900 text-white' : '',
                   'group flex w-full items-center rounded-md px-2 py-2 text-sm',
                 ]"
                 @click="userPreferences.updateLanguage(lang)"
